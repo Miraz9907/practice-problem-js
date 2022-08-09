9f26cc101036598f84e9f194649f7a488077a34d
Miraz9907 <meraz.rahman99@gmail.com>
2022-08-09 18:09:56 +0600

    second practice problem done from the first pdf

--- problem1.js
+++ problem1.js
@@ -1,4 +1,7 @@
 let momGave = 1000;
+// let orange = 300;
+// let apple =400;
+// let orangeApple = orange+apple;
 let orangeApple = 700;
 let shopkeeperReturn = momGave - orangeApple;
 console.log("Shopkeeper will return to Harry: ", shopkeeperReturn);
---
+++ problem2.js
@@ -0,0 +1,9 @@
+let mathematics = 90;
+let biology = 88;
+let chemistry = 85;
+let physics = 80;
+let bangla = 82;
+
+let totalMark = mathematics+biology+chemistry+physics+bangla;
+let averageMark = totalMark / 5;
+console.log(averageMark)

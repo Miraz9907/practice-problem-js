e776b45cbf3fe25d7e2b14e0ab2d94c2bc40ed2e
Miraz9907 <meraz.rahman99@gmail.com>
2022-08-09 22:15:31 +0600

    7th practice problem done from the first pdf

---
+++ problem7.js
@@ -0,0 +1,21 @@
+var miraz = 85;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+
+if(miraz > 80){
+    console.log("A grade");
+}
+else if(tom > 60){
+    console.log("B grade");
+}
+else if(jane > 50){
+    console.log("C grade");
+}
+else if(peter >=40){
+    console.log(peter, " got D grade");
+}
+else if(john <= 39){
+    console.log(tom, " got F grade");
+}

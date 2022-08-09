38011da54d368fc896a1f4096c57d5942eb029e5
Miraz9907 <meraz.rahman99@gmail.com>
2022-08-10 00:15:53 +0600

    11th practice problem done from the first pdf

---
+++ problem11.js
@@ -0,0 +1,15 @@
+
+var signal = "green";
+// console.log(signal);
+if(signal === "green"){
+    console.log("Now you can cross the road");
+}
+else if(signal === "yellow"){
+    console.log(" you should stop here");
+}
+
+else if(signal === "red"){
+    console.log("It could be danger.");
+}
+
+
